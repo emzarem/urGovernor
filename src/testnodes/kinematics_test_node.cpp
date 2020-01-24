4fa8da54d3e8753cefab7a1bb20c7e0abce9c6a5
emzarem <emurphyzaremba@gmail.com>
2020-01-24 15:04:47 -0500

    Initial commit

---
+++ src/testnodes/kinematics_test_node.cpp
@@ -0,0 +1,187 @@
+#include <ros/ros.h>
+
+// Shared lib
+#include "SerialPacket.h"
+
+// For kinematics
+#include "deltaRobot.h"
+
+// Srv and msg types
+#include <urGovernor/SerialWrite.h>
+#include <urGovernor/SerialRead.h>
+#include <urGovernor/KinematicsTest.h>
+
+// Parameters to read from configs
+std::string serialServiceWriteName;
+std::string serialServiceReadName;
+double endEffectorTime = 0;
+
+// Connections to Serial interface services
+ros::ServiceClient serialWriteClient;
+ros::ServiceClient serialReadClient;
+
+
+// General parameters for this node
+bool readGeneralParameters(ros::NodeHandle nodeHandle)
+{
+    if (!nodeHandle.getParam("serial_output_service", serialServiceWriteName)) return false;
+    if (!nodeHandle.getParam("serial_input_service", serialServiceReadName)) return false;
+    if (!nodeHandle.getParam("end_effector_time_s", endEffectorTime)) return false;
+   
+    return true;
+}
+
+/* 
+ * This is the main blocking call to set the arm position to the angles specified 
+ */
+bool actuateArmAngles(int angle1Deg, int angle2Deg, int angle3Deg)
+{
+    urGovernor::SerialWrite serialWrite;
+    urGovernor::SerialRead serialRead;   
+    SerialUtils::CmdMsg msg = {0};
+    std::vector<char> buff;
+
+    msg.is_relative = 0;
+    msg.m1_angle = angle1Deg;
+    msg.m2_angle = angle2Deg;
+    msg.m3_angle = angle3Deg;
+    msg.motors_done = 0;
+    
+    SerialUtils::pack(buff, msg);
+
+    serialWrite.request.command = std::string(buff.begin(), buff.end());
+
+    // Send angles to HAL (via calling the serial WRITE client)
+    if (serialWriteClient.call(serialWrite))
+    {
+        while (ros::ok())
+        {
+            // Wait for arm done
+            // This is done by calling the serial READ client
+            // This should block until we get a CmdMsg FROM the serial line
+            if (serialReadClient.call(serialRead))
+            {
+                std::vector<char> v(serialRead.response.command.begin(), serialRead.response.command.end());
+                
+                msg.motors_done = 0;
+                // Unpack response from read
+                SerialUtils::unpack(v, msg);
+
+                // This should indicate that we are done
+                if (msg.motors_done)
+                {
+                    return 1;
+                }
+                else 
+                {
+                    ROS_ERROR("Motors did not return with (motors_done == true)");
+                    return 0;
+                }
+            }
+            else
+            {
+                ROS_ERROR("Timed out waiting for response from Teensy ... retrying ...");
+            }
+
+            ros::spinOnce();
+        }
+    }
+    else
+    {
+        ROS_ERROR("Serial write to set motors was NOT successful.");
+    }
+    
+    return 0;
+}
+
+bool moveArm(urGovernor::KinematicsTest::Request &req, urGovernor::KinematicsTest::Response &res) {
+    // Create coordinates in the Delta Arm Reference
+    float x_coord = (float)req.x_coord;
+    float y_coord = (float)req.y_coord;
+    float z_coord = (float)req.z_coord;    // z = 0 IS AT THE GROUND (z = is always positive)
+
+    
+    /* Calculate angles for Delta arm */
+    robot_position(x_coord, x_coord, z_coord); 
+
+    int angle1Deg,angle2Deg,angle3Deg;
+
+    res.success = false; // default no success
+
+    // Get the resulting angles from kinematics
+    if (getArmAngles(&angle1Deg, &angle2Deg, &angle3Deg))
+    {
+        ROS_INFO("Delta for coords (%.2f,%.2f,%.2f) [cm] -> (%i,%i,%i) [degrees]",
+                    x_coord, y_coord, z_coord, angle1Deg, angle2Deg, angle3Deg);
+
+        // // Time the blocking call to actuate arm angles
+        // ros::WallTime start_, end_;
+        // start_ = ros::WallTime::now();
+        
+        // Block until we've actuated to these angles
+        if (actuateArmAngles(angle1Deg, angle2Deg, angle3Deg))
+        {
+            // Wait for end-effectors
+            // TODO: or send a different command?
+            ros::Duration(endEffectorTime).sleep();
+
+            // RESET ARM POSITIONS
+            if (!actuateArmAngles(0, 0, 0))
+            {
+                ROS_ERROR("Could not Reset arm positions.");
+                ros::requestShutdown();
+                return false;
+            } else {
+                res.success = true;
+            }
+        }
+        else
+        {
+            ROS_ERROR("Could not actuate motors to specified arm angles");
+            ros::requestShutdown();
+            return false;
+        }
+
+        // end_ = ros::WallTime::now();
+        // double execution_time = (end_ - start_).toNSec() * 1e-6;
+        // ROS_INFO_STREAM("Actuation time for weed (ms): " << execution_time);
+    }
+    else
+    {
+        ROS_ERROR("Could not get arm angles.");
+    }
+
+    return true;
+}
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "kinematics_test_node");
+    ros::NodeHandle nh;
+    ros::NodeHandle nodeHandle("~");
+
+    if (!readGeneralParameters(nodeHandle))
+    {
+        ROS_ERROR("Could not read general parameters for kinematics_test_node.");
+        ros::requestShutdown();
+    }
+
+    serialWriteClient = nh.serviceClient<urGovernor::SerialWrite>(serialServiceWriteName);
+    ros::service::waitForService(serialServiceWriteName);
+
+    serialReadClient = nh.serviceClient<urGovernor::SerialRead>(serialServiceReadName);
+    ros::service::waitForService(serialServiceReadName);
+
+    /* Initializing Kinematics */
+    // Set tool offset (tool id == 0, x, y, z )
+    robot_tool_offset(0, 0, 0, 0);
+    // Default deltarobot setup
+    deltarobot_setup();
+
+    ros::ServiceServer moveService = nodeHandle.advertiseService("move_to_coords", moveArm);
+
+    ros::spin();
+
+    return 0;
+}
